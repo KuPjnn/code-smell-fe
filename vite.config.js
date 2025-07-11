6e8b23b26c9bc933bb052e43e05df611b212d4a6
Code Smell <code.smell@gmail.com>
2025-07-11 14:05:35 +0700

    optimize to build

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig(({mode}) => {
     const env = loadEnv(mode, process.cwd(), '')
 
     return {
+        base: '/',
         plugins: [
             vue(),
             tailwindcss(),
